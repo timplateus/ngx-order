ab5e3f5e99acea434f706a1aafd94e1eee610468
Tim Plateus <timplateus@hotmail.com>
2019-08-23 23:12:39 +0200

    initial architecture

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RegistrationModule} from './registration/registration.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RegistrationModule
+  ]
+})
+export class PagesModule { }
