a6808c8d3dc6c6e9ceabe0e08c7e1e09b5e2b6de
Tim Plateus <timplateus@hotmail.com>
2023-08-15 18:44:09 +0200

    replace legacy material components

--- src/app/pages/order/order.module.ts
+++ src/app/pages/order/order.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
-import { MatLegacyTabsModule as MatTabsModule } from '@angular/material/legacy-tabs';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
 import { SharedModule } from '../../shared/shared.module';
 import { SummaryItemComponent } from './components/summary-item/summary-item.component';
 import { OrderPageComponent } from './order-page/order-page.component';
