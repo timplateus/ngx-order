056c839b35078b5e118512b0a5b9474c4a731c0a
Tim Plateus <timplateus@hotmail.com>
2025-11-25 14:02:02 +0100

    chore:provide routes directly

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,36 @@
-import { enableProdMode, importProvidersFrom, inject, provideAppInitializer } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode, inject, provideAppInitializer } from '@angular/core';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { mockInterceptor } from './app/shared/mocks/mock.interceptor';
-import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { AppConfigService } from './app/shared/services/app-config.service';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule),
-        provideAppInitializer(() => {
-        const initializerFn = ((appConfigService: AppConfigService) => {
-                return () => appConfigService.loadAppConfig();
-            })(inject(AppConfigService));
-        return initializerFn();
-      }),
-        provideHttpClient(withInterceptors(!environment.production ? [mockInterceptor] : [])),
-        provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi()),
-    ]
-})
-  .catch((err) => console.error(err));
+  providers: [
+    provideRouter(routes),
+    provideAppInitializer(() => {
+      const initializerFn = ((appConfigService: AppConfigService) => {
+        return () => appConfigService.loadAppConfig();
+      })(inject(AppConfigService));
+      return initializerFn();
+    }),
+    provideHttpClient(
+      withInterceptors(!environment.production ? [mockInterceptor] : []),
+    ),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+}).catch((err) => console.error(err));
